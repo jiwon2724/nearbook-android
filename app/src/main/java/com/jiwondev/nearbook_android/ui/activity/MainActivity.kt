0e76158b5449a07d77fcc56b8ae5242c426d7aed
jiwon2724 <stopone3119@naver.com>
2023-02-27 23:36:33 +0900

    chore : BottomNavigation, Fragment 매핑

--- app/src/main/java/com/jiwondev/nearbook_android/ui/activity/MainActivity.kt
+++ app/src/main/java/com/jiwondev/nearbook_android/ui/activity/MainActivity.kt
@@ -2,13 +2,21 @@ package com.jiwondev.nearbook_android.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.jiwondev.nearbook_android.R
+import com.jiwondev.nearbook_android.databinding.ActivityMainBinding
+import com.jiwondev.nearbook_android.databinding.ActivityNicknameBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it)}) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        init()
+    }
 
+    private fun init() {
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(host.navController)
     }
 }
---
+++ app/src/main/java/com/jiwondev/nearbook_android/ui/fragment/LibraryFragment.kt
@@ -0,0 +1,22 @@
+package com.jiwondev.nearbook_android.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jiwondev.nearbook_android.R
+
+
+class LibraryFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_library, container, false)
+    }
+}
