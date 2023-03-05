c1a9aa2226890094eed08b766a36e3d10ba4a097
jiwon2724 <stopone3119@naver.com>
2023-03-05 21:52:17 +0900

    chore : add Hilt Annotation, Data Layer Class

---
+++ app/src/main/java/com/jiwondev/nearbook_android/data/repository/SearchBookRepository.kt
@@ -0,0 +1,4 @@
+package com.jiwondev.nearbook_android.data.repository
+
+class SearchBookRepository {
+}
--- app/src/main/java/com/jiwondev/nearbook_android/ui/viewmodel/SearchBookViewModel.kt
+++ app/src/main/java/com/jiwondev/nearbook_android/ui/viewmodel/SearchBookViewModel.kt
@@ -1,5 +1,15 @@
 package com.jiwondev.nearbook_android.ui.viewmodel
 
-class SearchBookViewModel {
+import androidx.lifecycle.ViewModel
+import com.jiwondev.nearbook_android.data.datasource.SearchBookDataSource
+import com.jiwondev.nearbook_android.data.repository.SearchBookRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchBookViewModel @Inject constructor(
+    private val searchBookDataSource: SearchBookDataSource,
+    private val searchBookRepository: SearchBookRepository
+) : ViewModel() {
 
 }
